ad94db63923a2dc2b9144e3f8417c54f61ca0aa1
Remy-J <rjpro77@outlook.fr>
2024-02-18 18:58:33 +0100

    use flatList to display product list

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,11 +1,16 @@
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, FlatList } from 'react-native';
 import products from '@assets/data/products';
 import ProductListItem from '@/components/ProductListItem';
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[1]} />
+      <FlatList data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        numColumns={2}
+        contentContainerStyle={{ gap: 10 }}
+        columnWrapperStyle={{ gap: 10 }}
+      />
     </View>
   );
 }
