cb892997fda10f0fad04c12c7d2d1d9dc324e2f5
Remy-J <rjpro77@outlook.fr>
2024-02-18 18:45:44 +0100

    add typescript path and clean code

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,33 +1,11 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
-import products from '@/assets/data/products';
-import ProductListItem from '@/src/components/ProductListItem';
+import products from '@assets/data/products';
+import ProductListItem from '@/components/ProductListItem';
 
 export default function MenuScreen() {
   return (
     <View>
-      <ProductListItem product={products[1]}/>
+      <ProductListItem product={products[1]} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 20,
-  },
-  image: {
-    width: '100%',
-    aspectRatio: 1
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '600',
-    marginVertical: 10
-  },
-  price: {
-    color: Colors.light.tint,
-    fontWeight: 'bold'
-  },
-});
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View, Image } from 'react-native';
-import Colors from '@/src/constants/Colors';
+import Colors from '@constants/Colors';
 import { Product } from '../types';
 
-const ProductListItem: React.FC<{ product: Product }> = ({ product }) => {
+export const defaultProductImage = 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png'
+
+type ProductListItemProps = {
+  product: Product
+}
+
+
+const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: product.image! }} style={styles.image} />
+      <Image source={{ uri: product.image || defaultProductImage }} style={styles.image} />
       <Text style={styles.title}>{product.name}</Text>
       <Text style={styles.price}>${product.price}</Text>
     </View>
