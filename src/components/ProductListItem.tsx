27cb3f52a9d3a9721c4d43013c749e5e0716a8f4
Remy-J <rjpro77@outlook.fr>
2024-02-18 18:09:03 +0100

    create ProductListItem component

---
+++ src/components/ProductListItem.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Colors from '@/src/constants/Colors';
+
+
+interface IProduct {
+  id: number
+  image: string
+  name: string
+  price: number
+}
+
+const ProductListItem: React.FC<{ product: IProduct }> = ({ product }) => {
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={styles.title}>{product.name}</Text>
+      <Text style={styles.price}>${product.price}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 20,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 1
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: '600',
+    marginVertical: 10
+  },
+  price: {
+    color: Colors.light.tint,
+    fontWeight: 'bold'
+  },
+});
+
+export default ProductListItem
